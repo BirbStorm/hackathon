5b9a9291fedfe5d7f2c4d865c0192b27e3fb5370
Andy <russellandy98@gmail.com>
2020-03-07 09:40:50 -0500

    edited form to include result component

--- hackathon/pages/index.js
+++ hackathon/pages/index.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import fetch from 'isomorphic-unfetch'
+import dayjs from 'dayjs'
+import Result from '../components/result';
 
-const Home = () => (
+const Home = () => {
+  let data = {
+    calories: {
+      label: "Calories",
+      total: 1840,
+      target: 1840,
+      variant: 15
+    },
+    sleep: {
+      label: "Sleep",
+      total: 8,
+      target: 8,
+      variant: 2
+      // wake: '07:30:00',
+      // sleep: '22:00:00'
+    },
+    weight: {
+      label: "Weight",
+      total: 180,
+      target: 150,
+      variant: 20
+    },
+    exercise: {
+      label: "Exercise",
+      total: 30,
+      target: 60,
+      variant: 10
+    }
+  }
+  const [results, setResults] = useState(data)
+  return (
   <div>
     <Head>
       <title>Health tracker</title>
@@ -24,103 +56,30 @@ const Home = () => (
         <div class="w-1/3 bg-gray-200 p-4">Next Day</div>
       </div>
 
-      <div class="flex mb-4 text-center">
-        <div class="w-1/4 p-4 bg-green-500 text-white">
-          <h2 className="text-3xl font-bold">1850
-            <div class="flex text-sm p-4">
-              <div class="w-1/3">1700</div>
-              <div class="w-1/3 font-bold">1850</div>
-              <div class="w-1/3">2000</div>
-            </div>
-          </h2>
-          <h3 className="text-xl">Food</h3>
-        </div>
-        <div class="w-1/4 p-4 bg-red-500 text-white">
-          <h2 className="text-3xl font-bold">195
-            <div class="flex text-sm p-4">
-              <div class="w-1/3">150</div>
-              <div class="w-1/3 font-bold">160</div>
-              <div class="w-1/3">170</div>
-            </div>
-          </h2>
-          <h3 className="text-xl">Sleep</h3>
-        </div>
-        <div class="w-1/4 p-4 bg-green-500 text-white">
-          <h2 className="text-3xl font-bold">55
-            <div class="flex text-sm p-4">
-              <div class="w-1/3">50</div>
-              <div class="w-1/3 font-bold">60</div>
-              <div class="w-1/3">70</div>
-            </div>
-          </h2>
-          <h3 className="text-xl">Exercises</h3>
-        </div>
-        <div class="w-1/4 p-4 bg-blue-500 text-white">
-          <h2 className="text-3xl font-bold">120
-            <div class="flex text-sm p-4">
-              <div class="w-1/3">145</div>
-              <div class="w-1/3 font-bold">160</div>
-              <div class="w-1/3">175</div>
-            </div>
-          </h2>
-          <h3 className="text-xl">Weight</h3>
-        </div>
+      <div className="flex mb-4 text-center">
+        <Result results={results.calories} />
+        <Result results={results.sleep} />
+        <Result results={results.exercise} />
+        <Result results={results.weight} />
       </div>
 
       <div className="flex">
-        <div className="w-1/4">
-          <h2 className="text-3xl p-4">Food</h2>
+        <div className="w-1/3">
+          <h2 className="text-3xl p-4">Current</h2>
           <div className="p-4">
             <label className="block">Calories</label>
             <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
           </div>
           <div className="p-4">
-            <label className="block">Carbs</label>
+            <label className="block">Time Asleep</label>
             <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
           </div>
           <div className="p-4">
-            <label className="block">Fat</label>
+            <label className="block">Exercise Time</label>
             <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
           </div>
           <div className="p-4">
-            <label className="block">Protein</label>
-            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
-          </div>
-          <div className="p-4">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Save
-            </button>
-          </div>
-        </div>
-        <div className="w-1/4">
-          <h2 className="text-3xl p-4">Sleep</h2>
-          <div className="p-4">
-            <label className="block">Went to Sleep at</label>
-            <input type="time" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
-          </div>
-          <div className="p-4">
-            <label className="block">Woke up at</label>
-            <input type="time" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
-          </div>
-          
-          <div className="p-4">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Save
-            </button>
-          </div>
-        </div>
-        <div className="w-1/4">
-          <h2 className="text-3xl p-4">Exercise</h2>
-          <div className="p-4">
-            <label className="block">Time</label>
-            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
-          </div>
-          <div className="p-4">
-            <label className="block">Intensity</label>
-            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
-          </div>
-          <div className="p-4">
-            <label className="block">Calories Burnt</label>
+            <label className="block">Weight</label>
             <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
           </div>
           <div className="p-4">
@@ -129,24 +88,59 @@ const Home = () => (
             </button>
           </div>
         </div>
-        <div className="w-1/4">
-        <div className="w-1/4">
-          <h2 className="text-3xl p-4">Weight</h2>
+        <div className="w-1/3">
+          <h2 className="text-3xl p-4">Target</h2>
           <div className="p-4">
-            <label className="block">Current Weight</label>
+            <label className="block">Calories</label>
             <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
           </div>
-          
+          <div className="p-4">
+            <label className="block">Time Asleep</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
+          <div className="p-4">
+            <label className="block">Exercise Time</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
+          <div className="p-4">
+            <label className="block">Weight</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
           <div className="p-4">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Save
             </button>
           </div>
         </div>
+        <div className="w-1/3">
+          <h2 className="text-3xl p-4">Variant</h2>
+          <div className="p-4">
+            <label className="block">Calories</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
+          <div className="p-4">
+            <label className="block">Time Asleep</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
+          <div className="p-4">
+            <label className="block">Exercise Time</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
+          <div className="p-4">
+            <label className="block">Weight</label>
+            <input type="number" className="bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></input>
+          </div>
+          <div className="p-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Save
+            </button>
+          </div>
         </div>
+
       </div>
     </div>
   </div>
-)
+  )
+}
 
 export default Home
